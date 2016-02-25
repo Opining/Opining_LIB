a3ba4e936ff89c9b586ff3427e46b97798947d37
José Renan <joserenansl99@gmail.com>
2016-02-25 00:16:39 -0300

    Updated the src_client

---
+++ src_client/br/com/opining/library/client/model/room/polarized/NewPolarizedRoom.java
@@ -0,0 +1,52 @@
+package br.com.opining.library.client.model.room.polarized;
+
+import br.com.opining.library.client.model.User;
+
+public class NewPolarizedRoom {
+	
+	private String newSubject;
+	private Integer newArgumentTimeInMinutes;
+	private User creator;
+	private Integer newNumMaxParticipants;
+	private String newPassword;
+	
+	public String getNewSubject() {
+		return newSubject;
+	}
+	
+	public void setNewSubject(String newSubject) {
+		this.newSubject = newSubject;
+	}
+	
+	public Integer getNewArgumentTimeInMinutes() {
+		return newArgumentTimeInMinutes;
+	}
+	
+	public void setNewArgumentTimeInMinutes(Integer newArgumentTimeInMinutes) {
+		this.newArgumentTimeInMinutes = newArgumentTimeInMinutes;
+	}
+	
+	public User getNewCreator() {
+		return creator;
+	}
+	
+	public void setNewCreator(User newCreator) {
+		this.creator = newCreator;
+	}
+	
+	public Integer getNewNumMaxParticipants() {
+		return newNumMaxParticipants;
+	}
+	
+	public void setNewNumMaxParticipants(Integer newNumMaxParticipants) {
+		this.newNumMaxParticipants = newNumMaxParticipants;
+	}
+	
+	public String getNewPassword() {
+		return newPassword;
+	}
+	
+	public void setNewPassword(String newPassword) {
+		this.newPassword = newPassword;
+	}
+}
