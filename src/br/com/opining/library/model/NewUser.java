81a84658afaa27d720489abd46d7e6a2334c2d9b
José Renan <joserenansl99@gmail.com>
2016-05-20 22:40:43 -0300

    Corrigidos bugs no NewUser

--- src/br/com/opining/library/model/NewUser.java
+++ src/br/com/opining/library/model/NewUser.java
@@ -4,6 +4,8 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 import br.com.opining.library.model.User;
+import br.com.opining.library.model.error.Errors;
+import br.com.opining.library.model.error.OpiningValidateException;
 
 @XmlRootElement
 public class NewUser {
@@ -38,8 +40,18 @@ public class NewUser {
 		return newLogin;
 	}
 
-	public void setNewLogin(String newLogin) {
-		this.newLogin = newLogin;
+	public void setNewLogin(String login) {
+		
+		if (!login.matches("^[a-z0-9]+$"))
+			throw new OpiningValidateException(Errors.LOGIN_FORMAT_NOT_ACCEPTED);
+		
+		if (login == null || login.length() < User.LOGIN_MIN_LENGHT)
+			throw new OpiningValidateException(Errors.LOGIN_IS_TOO_SHORT);
+		
+		if (login.length() > User.LOGIN_MAX_LENGHT)
+			throw new OpiningValidateException(Errors.LOGIN_IS_TOO_LONG);
+		
+		this.newLogin = login;
 	}
 
 	@XmlElement
@@ -47,8 +59,15 @@ public class NewUser {
 		return newName;
 	}
 
-	public void setNewName(String newName) {
-		this.newName = newName;
+	public void setNewName(String name) {
+		
+		if (name.length() > User.NAME_MAX_LENGHT)
+			throw new OpiningValidateException(Errors.NAME_IS_TOO_LONG);
+		
+		if (name == null || name.length() < User.NAME_MIN_LENGHT)
+			throw new OpiningValidateException(Errors.NAME_IS_TOO_SHORT);
+		
+		this.newName = name;
 	}
 
 	@XmlElement
